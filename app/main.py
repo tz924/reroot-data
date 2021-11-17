92945eefb2dc52c8d1d6a40a637e7d9d0f0be544
Thomas Zhang <zhj930924@gmail.com>
2021-11-17 07:21:39 -0800

    cleaned up methods

--- app/main.py
+++ app/main.py
@@ -54,11 +54,35 @@ def normalize_query(params):
     return {k: normalize_query_param(v) for k, v in params_non_flat.items()}
 
 
-def get_scores(request):
+def grouped_to_dict(grouped):
+    """
+    helper function to convert table to nested dictionary
+    """
+    results = defaultdict(lambda: defaultdict(dict))
+
+    for index, value in grouped.itertuples():
+        for i, key in enumerate(index):
+            if i == 0:
+                nested = results[key]
+            elif i == len(index) - 1:
+                nested[key] = value
+            else:
+                nested = nested[key]
+    return results
+
+
+    #
+# create the flask app
+app = Flask(__name__)
+
+
+@app.route('/scores')
+def return_scores():
     """
     calculate scores for all counties given input arguments
     """
-    parameter_vars = [x for x in request if request[x]]
+    inputs = request.args
+    parameter_vars = [x for x in inputs if inputs[x]]
     rank_vars = [x.replace("input_", "rank_") for x in parameter_vars]
     vars = [x.replace("input_", "") for x in parameter_vars]
 
@@ -74,33 +98,23 @@ def get_scores(request):
     return json.dumps(score_results)
 
 
-# get all data for input counties
-def get_counties(request):
-    counties = request.get('counties')
+@app.route('/counties')
+def return_counties():
+    """
+    # get all data for input counties
+    """
+    counties = request.args.get('counties')
     county_results = data[[str(x) in counties for x in data.county_code]]
     county_results = county_results.set_index(
         'county_code').transpose().to_dict()
     return jsonify(county_results)
 
 
-# helper function to convert table to nested dictionary
-def grouped_to_dict(grouped):
-    results = defaultdict(lambda: defaultdict(dict))
-
-    for index, value in grouped.itertuples():
-        for i, key in enumerate(index):
-            if i == 0:
-                nested = results[key]
-            elif i == len(index) - 1:
-                nested[key] = value
-            else:
-                nested = nested[key]
-
-    return results
-
-
-# get all parameters based on parameters table
-def get_parameters():
+@app.route('/parameters')
+def return_parameters():
+    """
+    get all parameters based on parameters table
+    """
     grouped_data = parameters[['category',
                                'subcategory', 'field', 'variable_name']]
     grouped_data = grouped_data.groupby(['category', 'subcategory', 'field']).agg(
@@ -108,35 +122,14 @@ def get_parameters():
     return json.dumps(grouped_to_dict(grouped_data))
 
 
-# get list of all country codes and all country names
-def get_all_counties():
+@app.route('/all_counties')
+def return_all_counties():
+    """
+    get list of all country codes and all country names
+    """
     all_county_results = data[['county_code',
                                'county_name']].to_dict(orient='list')
     return json.dumps(all_county_results)
-
-
-# create the flask app
-app = Flask(__name__)
-
-
-@app.route('/scores')
-def return_scores():
-    return get_scores(request.args)
-
-
-@app.route('/counties')
-def return_counties():
-    return get_counties(request.args)
-
-
-@app.route('/parameters')
-def return_parameters():
-    return get_parameters()
-
-
-@app.route('/all_counties')
-def return_all_counties():
-    return get_all_counties()
 
 
 if __name__ == '__main__':
